b3db326019e3a9cfc62896d79bf687cd939437a0
Nilton Oliveira <jniltinho@gmail.com>
2024-05-06 18:57:40 -0300

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -9,33 +9,12 @@ import argparse
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-
-from rocket import Rocket
 from app import app
-
-
 
 ## Clean .pyc
 os.system("find . -type f -iname *.pyc -exec rm  -f {} \;")
 os.system("echo 1 > http.log")
-
-
-def rocket_http(debug=False):
-    # Setup logging
-    log = logging.getLogger('Rocket')
-    log.setLevel(logging.INFO)
-    if debug:
-       log.addHandler(logging.StreamHandler(sys.stdout))
-    else:
-       log.addHandler(logging.FileHandler('http.log'))
-
-    # Set the configuration of the web server
-    server = Rocket(interfaces=('0.0.0.0', 8010, 'ssl/server.key', 'ssl/server.crt'), method='wsgi', app_info={"wsgi_app": app})
-    server.start(background=True)
-
 
 
 def flask_http(debug=False, ssl=False):
@@ -47,8 +26,6 @@ def flask_http(debug=False, ssl=False):
         app.run(host='0.0.0.0', port=8010, debug=debug, ssl_context=context)
     else:
         app.run(host='0.0.0.0', port=8010, debug=debug)
-
-
 
 
 def main():
@@ -68,7 +45,7 @@ def main():
        flask_http(debug=args.debug, ssl=args.ssl)
        sys.exit(1)
 
-    rocket_http(debug=args.debug)
+    flask_http(debug=args.debug, ssl=args.ssl)
 
 
 
